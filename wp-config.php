0e364e7f02695c8f221559e4d955198b4e828ac2
Gerardo Forero <gforeroh@gmail.com>
2025-11-24 15:07:03 -0500

    Ajustre

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'db_tutorial_web' );
+define( 'DB_NAME', 'db_libranza_oportunidades_web' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
